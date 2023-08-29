08bec13414c55f4e502fc3fb617352be8bbc9092
ANANTH-SWAMY <ananth.gnr@gmail.com>
2023-08-29 22:59:51 +0530

    domains and half of testimonials

---
+++ script.js
@@ -0,0 +1,31 @@
+let testimonials=[
+	{
+		name: "William James.",
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	},
+	{
+		name: "Noah Page.",
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	},
+	{
+		name: "Valentino Rossi."
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	},
+	{
+		name: "Tom Kristensen.",
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	},
+	{
+		name: "Nigel Mansell.",
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	},
+	{
+		name: "Marc Marquez.",
+		message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venematis, lectus"
+	}
+]
+
+function renderTestimonials(){
+	let carousel=document.getElementById("testimonials-carousel")
+
+}
